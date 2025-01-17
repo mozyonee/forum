19d97f06d0eed79b95e89c52ed74bc72f0cda9cc
mozyonee <leslieleecoleman@gmail.com>
2025-01-17 15:37:37 +0100

    update package.json and server/src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,7 @@ import compression from 'compression';
 import mongoose from 'mongoose';
 import routerV1 from './routerV1';
 import cors from 'cors';
-import ServerlessHttp from 'serverless-http';
+// import ServerlessHttp from 'serverless-http';
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
@@ -27,15 +27,15 @@ app.use(bodyParser.json());
 
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI);
-// mongoose.connection.once('open', () => {
-// 	const server = http.createServer(app);
-// 	const { SERVER_HOST, SERVER_PORT } = process.env;
-// 	server.listen(SERVER_PORT, () => {
-// 		console.log(`server listened on ${SERVER_HOST}:${SERVER_PORT}`);
-// 	})
-// });
+mongoose.connection.once('open', () => {
+	const server = http.createServer(app);
+	const { SERVER_HOST, SERVER_PORT } = process.env;
+	server.listen(SERVER_PORT, () => {
+		console.log(`server listened on ${SERVER_HOST}:${SERVER_PORT}`);
+	})
+});
 mongoose.connection.on('error', (error: Error) => console.log(error));
 
 app.use('/v1', routerV1());
 
-export const handler = ServerlessHttp(app);
+// export const handler = ServerlessHttp(app);
