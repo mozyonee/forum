6b51802bb323d423e2e35d96a1fe750f9892710b
Vadym Abakumov <leslieleecoleman@gmail.com>
2025-03-14 22:53:40 +0100

    environment

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: `${process.env.SERVER_URL || 'http://localhost:8080'}/v1`,
+	baseURL: `${process.env.SERVER_URL}/v1`,
 	withCredentials: true
 });
 
