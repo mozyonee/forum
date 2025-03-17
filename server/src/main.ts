bc9bde16dd88ee8aee6b8e95c316b23815c69f8f
Vadym Abakumov <leslieleecoleman@gmail.com>
2025-03-17 16:07:21 +0100

    old cors client

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,25 +7,25 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	const configService = app.get(ConfigService);
 
-	app.use((req, res, next) => {
-		res.header('Access-Control-Allow-Origin', configService.get<string>('CLIENT_URL'));
-		res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-		res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-		next();
-	});
-
-	app.enableCors({
-		allowedHeaders: "*",
-		origin: configService.get<string>('CLIENT_URL'),
-		credentials: true
-	});
+	// app.use((req, res, next) => {
+	// 	res.header('Access-Control-Allow-Origin', configService.get<string>('CLIENT_URL'));
+	// 	res.header('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+	// 	res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+	// 	next();
+	// });
 
 	// app.enableCors({
-	// 	origin: true,
-	// 	credentials: true,
-	// 	methods: "GET,POST,PATCH,DELETE",
-	// 	allowedHeaders: ["Content-Type", "Accept", 'Authorization']
+	// 	allowedHeaders: "*",
+	// 	origin: configService.get<string>('CLIENT_URL'),
+	// 	credentials: true
 	// });
+
+	app.enableCors({
+		origin: configService.get<string>('CLIENT_URL'),
+		credentials: true,
+		methods: "GET,POST,PATCH,DELETE",
+		allowedHeaders: ["Content-Type", "Accept", 'Authorization']
+	});
 
 	app.useLogger(new Logger());
 
@@ -38,6 +38,8 @@ async function bootstrap() {
 	);
 
 	app.setGlobalPrefix('v1');
+
+	console.log(configService.get(<string>("CLIENT_URL")));
 
 	await app.listen(8080, '0.0.0.0');
 }
