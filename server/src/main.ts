2a10db0cdf470b650f749d1e80c04bbc4f45480e
Vadym Abakumov <leslieleecoleman@gmail.com>
2025-03-17 16:11:20 +0100

    cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -22,9 +22,23 @@ async function bootstrap() {
 
 	app.enableCors({
 		origin: configService.get<string>('CLIENT_URL'),
-		credentials: true,
 		methods: "GET,POST,PATCH,DELETE",
-		allowedHeaders: ["Content-Type", "Accept", 'Authorization']
+		allowedHeaders: ["Content-Type", "Accept", 'Authorization'],
+		credentials: true
+	});
+
+	app.use((req, res, next) => {
+		res.header("Access-Control-Allow-Origin", configService.get<string>('CLIENT_URL')); // Must be an exact URL, not "*"
+		res.header("Access-Control-Allow-Credentials", "true");
+		res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+		res.header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization");
+
+		// Handle preflight (OPTIONS) requests
+		if (req.method === "OPTIONS") {
+			return res.sendStatus(204);
+		}
+
+		next();
 	});
 
 	app.useLogger(new Logger());
